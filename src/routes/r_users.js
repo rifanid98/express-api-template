f82c78cf0fa6474b9a70e47375479977bed1e6c8
rifanid98 <adninsijawa.office@gmail.com>
2020-08-07 18:57:30 +0700

    users route example

---
+++ src/routes/r_users.js
@@ -0,0 +1,38 @@
+/**
+ * ExpressJs FW
+ */
+// import express framework
+const express = require('express');
+
+// instance of express router
+const router = express.Router();
+
+// import multer upload helper
+const upload = require('../helpers/upload');
+
+/**
+ * Controllers
+ */
+// load the controller
+const usersController = require('../controllers/c_users');
+
+
+/**
+ * Main CRUD
+ */
+// Get All Users
+router.get('/', usersController.getUsers);
+// Post a User
+router.post('/', upload.single('image'), usersController.postUser);
+// Patch a User
+router.patch('/:id', upload.single('image'), usersController.patchUser);
+// Delete a User
+router.delete('/:id', usersController.deleteUser);
+
+/**
+ * Ext CRUD
+ */
+// Get User By ID
+router.get('/:id', usersController.getUserById);
+
+module.exports = router;
